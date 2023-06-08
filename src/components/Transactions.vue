2130d689bd1336252b81aeae6015f5e45c406345
CodeInject88 <89700674+CodeInject88@users.noreply.github.com>
2023-06-08 09:52:07 +0200

    Update Transactions.vue
    Add transaction cache

--- src/components/Transactions.vue
+++ src/components/Transactions.vue
@@ -2,40 +2,59 @@
   <div class="container">
     <h5>Transactions</h5>
     <table class="table">
-    <thead>
-      <tr>
-        <th>Account</th>
-        <th>Date</th>
-        <th>Payee</th>
-        <th>Category</th>
-        <th>Memo</th>
-        <th>Amount</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="transaction in transactions" v-bind:key="transaction.id">
-        <td>{{transaction.account_name}}</td>
-        <td>{{transaction.date}}</td>
-        <td>{{transaction.payee_name}}</td>
-        <td>{{transaction.category_name}}</td>
-        <td>{{transaction.memo}}</td>
-        <td>{{convertMilliUnitsToCurrencyAmount(transaction.amount).toFixed(2)}}</td>
-      </tr>
-    </tbody>
+      <thead>
+        <tr>
+          <th>Account</th>
+          <th>Date</th>
+          <th>Payee</th>
+          <th>Category</th>
+          <th>Memo</th>
+          <th>Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="transaction in cachedTransactions" v-bind:key="transaction.id">
+          <td>{{ transaction.account_name }}</td>
+          <td>{{ transaction.date }}</td>
+          <td>{{ transaction.payee_name }}</td>
+          <td>{{ transaction.category_name }}</td>
+          <td>{{ transaction.memo }}</td>
+          <td>{{ convertMilliUnitsToCurrencyAmount(transaction.amount).toFixed(2) }}</td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
 
 <script>
-// Import utils from YNAB
-import {utils} from 'ynab';
+  import { utils } from 'ynab';
 
-export default {
-  props: ['transactions'],
-  methods: {
-    // Now we can make this method available to our template
-    // So we can format this milliunits in the correct currency format
-    convertMilliUnitsToCurrencyAmount: utils.convertMilliUnitsToCurrencyAmount
-  }
-}
+  export default {
+    props: ['transactions'],
+    data() {
+      return {
+        cachedTransactions: [],
+        cacheTimestamp: null
+      };
+    },
+    created() {
+      this.buildCache();
+    },
+    methods: {
+      convertMilliUnitsToCurrencyAmount: utils.convertMilliUnitsToCurrencyAmount,
+      buildCache() {
+        if (this.shouldRebuildCache()) {
+          this.cachedTransactions = this.transactions;
+          this.cacheTimestamp = Date.now();
+        }
+      },
+      shouldRebuildCache() {
+        return (
+          this.cachedTransactions.length === 0 ||
+          this.cacheTimestamp === null ||
+          Date.now() - this.cacheTimestamp >= 600000
+        );
+      }
+    }
+  };
 </script>
